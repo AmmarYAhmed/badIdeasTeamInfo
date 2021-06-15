88dd677bb72637e35d0d6cc6b38a1dcbcf13ffdb
Aaron Liu <aaronliunj@gmail.com>
2021-06-15 14:16:41 -0500

    Console Output(Activity 4)

--- badIdeasTeamInfo/badIdeasTeamInfo/Program.cs
+++ badIdeasTeamInfo/badIdeasTeamInfo/Program.cs
@@ -7,6 +7,9 @@ namespace badIdeasTeamInfo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Aaron Liu");
+            Console.WriteLine("Brookfield Central High School");
+            Console.WriteLine("A pair of socks");
         }
     }
 }
