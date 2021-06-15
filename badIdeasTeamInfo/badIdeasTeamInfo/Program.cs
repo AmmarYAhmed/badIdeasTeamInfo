0eb23dd4ebd4de735c6cb3dc756b03a8a145d47d
Elijah Bansley <elijahbansley@gmail.com>
2021-06-15 14:08:37 -0500

    Updated console output

--- badIdeasTeamInfo/badIdeasTeamInfo/Program.cs
+++ badIdeasTeamInfo/badIdeasTeamInfo/Program.cs
@@ -7,6 +7,9 @@ namespace badIdeasTeamInfo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm Elijah Bansley");
+            Console.WriteLine("I will likely be going to Monroe High School next year");
+            Console.WriteLine("I can't remember any particularly bad gift i've gotten from anyone");
         }
     }
 }
